d7ae5a3adb09bba8ed5a3499b2a06f2238635be8
Albert <webbhelp_@hotmail.com>
2016-10-06 15:35:46 +0200

    Only run once, and better event names

--- ScrollHandler.js
+++ ScrollHandler.js
@@ -1,22 +1,27 @@
 var ScrollHandler = {
+    active: false,
     element: null,
     scrollEvent: null,
     onStopTimer: null,
     options: {},
     initialize: function($element, options) {
+        if (ScrollHandler.active == true) {
+            return true;
+        }
         var options = options || {};
         options = $.extend(ScrollHandler.options,  options);
 
         ScrollHandler.element = $element;
         ScrollHandler.scrollEvent = ScrollHandler.element.on('scroll', ScrollHandler.onScroll);
+        ScrollHandler.active = true;
     },
     onScroll: function() {
-        ScrollHandler.element.trigger('onScroll');
+        ScrollHandler.element.trigger('ScrollHandler-Scroll')
         //On Scroll Stop
         clearTimeout(ScrollHandler.onStopTimer);
         ScrollHandler.onStopTimer = setTimeout(ScrollHandler.onScrollStop, 500);
     },
     onScrollStop: function() {
-        ScrollHandler.element.trigger('scrollStop');
+        ScrollHandler.element.trigger('scrollHandler-ScrollStop');
     }
 };
