2ad88b9b12fd7ddf08fb4a2306cae669e455320e
Albert <webbhelp_@hotmail.com>
2016-11-16 16:27:09 +0100

    Improvements

--- ScrollHandler.js
+++ ScrollHandler.js
@@ -3,25 +3,53 @@ var ScrollHandler = {
     element: null,
     scrollEvent: null,
     onStopTimer: null,
-    options: {},
-    initialize: function($element, options) {
+    callbacks: {
+        onScroll: [],
+        onScrollStop: [],
+    },
+    initialize: function($element) {
         if (ScrollHandler.active == true) {
             return true;
         }
-        var options = options || {};
-        options = $.extend(ScrollHandler.options,  options);
 
-        ScrollHandler.element = $element;
-        ScrollHandler.scrollEvent = ScrollHandler.element.on('scroll', ScrollHandler.onScroll);
+        ScrollHandler.setElement($element);
+        
         ScrollHandler.active = true;
+        $(document).trigger('ScrollHandler-Initialized')
     },
     onScroll: function() {
         ScrollHandler.element.trigger('ScrollHandler-Scroll')
         //On Scroll Stop
         clearTimeout(ScrollHandler.onStopTimer);
         ScrollHandler.onStopTimer = setTimeout(ScrollHandler.onScrollStop, 300);
+
+        var callbacks = ScrollHandler.callbacks.onScroll;
+        for (var i in callbacks) {
+            callbacks[i](ScrollHandler.element);
+        }
     },
     onScrollStop: function() {
         ScrollHandler.element.trigger('ScrollHandler-ScrollStop');
+        var callbacks = ScrollHandler.callbacks.onScrollStop;
+        for (var i in callbacks) {
+            callbacks[i](ScrollHandler.element);
+        }
+    },
+    bindScrollEvent: function() {
+        ScrollHandler.element.on('scroll', ScrollHandler.onScroll);
+    },
+    unbindScrollEvent: function() {
+        ScrollHandler.element.unbind('scroll');
+    },
+    setElement: function(element) {
+        ScrollHandler.unbindScrollEvent();
+        ScrollHandler.element = element;
+        ScrollHandler.bindScrollEvent();
+    },
+    callback: function(callback, type) {
+        var type = type || 'onScroll';
+        if (typeof callback == 'function' && typeof ScrollHandler.callbacks[type] != 'undefined') {
+            ScrollHandler.callbacks[type].push(callback);
+        }
     }
 };
